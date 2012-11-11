620c2f1e5bba3f1b9dafa519a6036ed4031adbdc
Fernando Jiménez <ferjmoreno@gmail.com>
2012-11-11 13:57:02 +0000

    Install me

---
+++ js/hackme.js
@@ -0,0 +1,17 @@
+
+function installMe() {
+  var req = navigator.mozApps.install(document.location.protocol + '//' +
+                                      document.location.host +
+                                      'firefoxos-hackme/manifest.webapp');
+  req.onsuccess = function onsucess() {
+    console.log('Yay! \o/');
+  };
+
+  req.onerror = function onerror(evt) {
+    console.log('Oh crap! + ' + evt.target.error.name);
+  };
+}
+
+window.addEventListener('load', function init(evt) {
+  document.getElementById('installApp').addEventListener('click', installApp);
+});
