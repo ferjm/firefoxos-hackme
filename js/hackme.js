903746f1d8de22ff3d2a8200805b4613fe0f891a
Fernando Jiménez <ferjmoreno@gmail.com>
2012-11-11 14:02:34 +0000

    Fixes

--- js/hackme.js
+++ js/hackme.js
@@ -1,14 +1,14 @@
 
 function installMe() {
   var req = navigator.mozApps.install(document.location.protocol + '//' +
-                                      document.location.host +
+                                      document.location.host + '//' +
                                       'firefoxos-hackme/manifest.webapp');
   req.onsuccess = function onsucess() {
     console.log('Yay! \o/');
   };
 
   req.onerror = function onerror(evt) {
-    console.log('Oh crap! + ' + evt.target.error.name);
+    console.log('Oh crap! ' + evt.target.error.name);
   };
 }
 
